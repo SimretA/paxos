be10cbdb5e358c31f26882b43e384c73ce450d3c
SimretA <simretaraya7@gmail.com>
2019-12-29 15:37:32 +0300

    proposer added

--- src/main.go
+++ src/main.go
@@ -2,7 +2,7 @@ package main
 
 //var wg sync.WaitGroup
 
-func main() {
+func main0() {
 	nodeServers := CreateServer(5)
 	m1 := value{from: 3, to: 1, typ: 1, seq: 1, preSeq: 0, val: "m1"}
 	nodeServers.sendMessage(m1)
@@ -12,7 +12,7 @@ func main() {
 	//	nodeServers.receiveMessage(1)
 	//	//defer wg.Done()
 	//}()
-	for i:= 0; i<5; i++{
+	for i := 0; i < 5; i++ {
 		recv(nodeServers, i)
 	}
 	//recv(nodeServers, 0)
@@ -20,6 +20,22 @@ func main() {
 	//wg.Wait()
 
 }
-func recv( nodes *servers,node int) {
+func main() {
+	nodeServers := CreateServer(5)
+	var acceptors []acceptor
+	aId := 1
+	for aId < 5 {
+		accptr := CreateAcceptor(aId, nodeServers)
+		acceptors = append(acceptors, accptr)
+		aId++
+	}
+	prop := CreateProposer(0, "value", nodeServers, 1, 2, 3, 4)
+	prop.run()
+
+	for index, _ := range acceptors {
+		go acceptors[index].run()
+	}
+}
+func recv(nodes *servers, node int) {
 	nodes.receiveMessage(node)
 }
--- src/proposer.go
+++ src/proposer.go
@@ -1,16 +1,90 @@
 package main
 
+import (
+	"fmt"
+	"log"
+)
+
+func CreateProposer(id int, val string, srvr *servers, acceptors ...int) *proposer {
+	prop := proposer{id: id, proposeVal: val, seq: 0, srvr: srvr}
+	prop.acceptors = make(map[int]value, len(acceptors))
+
+	for _, acceptor := range acceptors {
+		prop.acceptors[acceptor] = value{}
+	}
+	return &prop
+}
+
 type proposer struct {
 	id         int
 	seq        int
 	proposeNum int
 	proposeVal string
-	promiseCount int
 	acceptors  map[int]value
-	srvr         *servers
+	srvr       *servers
 }
 
 func (p *proposer) run() {
+	for !(p.getRecevPromiseCount() > p.majority()) {
+		sendMsgs := p.prepare()
+		for _, val := range sendMsgs {
+			p.srvr.sendMessage(val)
+			fmt.Println("propose sent")
+		}
 
+		m := p.srvr.receiveMessage(p.id)
+		if m == nil {
+			log.Println("[Proposer: no msg... ")
+			continue
+		}
+		log.Println("[Proposer: recev", m)
+		switch m.typ {
+		case 2: //for promise
+			log.Println(" proposer recev a promise from ", m.from)
+			p.checkReceivePromise(*m)
+		default:
+			panic("Unsupport message.")
+		}
+
+	}
 }
 
+func (p *proposer) majority() int {
+	return len(p.acceptors)/2 + 1
+}
+func (p *proposer) getRecevPromiseCount() int {
+	recvCount := 0
+	for _, acepMsg := range p.acceptors {
+		log.Println(" proposer has total ", len(p.acceptors), " acceptor ", acepMsg, " current Num:", p.getNewSeq(), " msgNum:", acepMsg.seq)
+		if acepMsg.seq == p.getNewSeq() {
+			log.Println("recv ++", recvCount)
+			recvCount++
+		}
+	}
+	log.Println("Current proposer recev promise count=", recvCount)
+	return recvCount
+}
+
+func (p *proposer) prepare() []value {
+	p.seq++
+	msgCount := 0
+	var msgList []value
+	for acepId, _ := range p.acceptors {
+		val := value{from: p.id, to: acepId, typ: 1, seq: p.getNewSeq(), val: p.proposeVal}
+		msgList = append(msgList, val)
+		msgCount++
+		if msgCount > p.majority() {
+			break
+		}
+	}
+	return msgList
+}
+
+func (p *proposer) getNewSeq() int {
+	p.proposeNum = p.seq<<4 | p.id
+	return p.proposeNum
+}
+
+func (p *proposer) checkReceivePromise(v value) {
+
+}
